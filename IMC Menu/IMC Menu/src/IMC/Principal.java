f5231eadf55891ca1ec94ba451b9b9d5925b0f42
Edukaxs <edunascimento605@gmail.com>
2026-08-03 17:58:15 -0300

    Delete IMC Menu directory

--- IMC Menu/IMC Menu/src/IMC/Principal.java
+++
@@ -1,9 +0,0 @@
-package IMC;
-import javax.swing.*;
-
-public class Principal {
-    public static void main(String[] args) {
-        MenuIMC menu = new MenuIMC();
-        menu.executarIMC();
-    }
-}
